25a73385b9b1766132b6ba41fd2c0f70e09982c1
Jernej <jrencelj@gmail.com>
2023-03-18 21:05:10 +0100

    Še vedno stara ideja, ne vem kako izboljšati z R in L.

--- naloga10.py
+++ naloga10.py
@@ -1,31 +1,53 @@
-# def najdaljse_podzaporedjeV2(tab):
-#     d = len(tab)
-#     def pomozna(z, k, m):
-#         print(z, k, m)
-#         tabele = []
-#         if z == k:
-#             if tab[z] > m:
-#                 memo[(z, k, m)] = 1
-#             else:
-#                 memo[(z, k, m)] = 0
-#         if k < z:
-#             return 0
-#         if (z, k, m) in memo.keys():
-#             return memo[(z, k, m)]
-#         t = 0
-#         for i in range(z, k + 1):
-#             if tab[i] < m:
-#                 continue
-#             if (i + 1) % 2 == 0:
-#                 t = 1 + pomozna(0, i - 1, tab[i])
-#             elif (i + 1) % 2 == 1:
-#                 t = 1 + pomozna(i + 1, d - 1, tab[i])
-#             tabele.append(t)
-#         memo[(z, k, m)] = max(tabele) if tabele != [] else 0
-#         print(memo)
-#         return memo[(z, k, m)]
-#     memo = {}
-#     return pomozna(0, d - 1, -float("inf"))
+def najdaljse_podzaporedjeV2(tab):
+    d = len(tab)
+    def pomozna(z, k, m):
+        print(z, k, m)
+        tabele = []
+        if z == k:
+            if tab[z] > m:
+                memo[(z, k, m)] = 1
+            else:
+                memo[(z, k, m)] = 0
+        if k < z:
+            return 0
+        if (z, k, m) in memo.keys():
+            return memo[(z, k, m)]
+        t = 0
+        for i in range(z, k + 1):
+            if tab[i] < m:
+                continue
+            if (i + 1) % 2 == 0:
+                t = 1 + pomozna(0, i - 1, tab[i])
+            elif (i + 1) % 2 == 1:
+                t = 1 + pomozna(i + 1, d - 1, tab[i])
+            tabele.append(t)
+        memo[(z, k, m)] = max(tabele) if tabele != [] else 0
+        print(memo)
+        return memo[(z, k, m)]
+    memo = {}
+    return pomozna(0, d - 1, -float("inf"))
+
+def najdaljse_naprej_nazaj_zaporedje(tab):
+    n = len(tab)
+    # def L(i):
+    #     for j in range(i):
+    # 
+    #     return None
+    # def R(i):
+    #     return None
+    def pomozna(i):
+        if i >= n:
+            return 0
+        if (i) in memo.keys():
+            return memo[i]
+        for i in range(n):
+            if (i + 1) % 2 == 1:
+                continue # Gledamo desne.
+            elif (i + 1) % 2 == 0:
+                continue # Gledamo leve.
+
+    memo = {}
+    return pomozna(0)
 
 def najdaljse_podzaporedje(tab):
     d = len(tab)
